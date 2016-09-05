1f9d56af8a35f1e16ea4a3f07f4249f79da00791
GauravB159 <gbolosta@gmail.com>
2016-09-06 01:57:29 +0530

    Added Reset, Backspace and Collapsed all input functions into one

---
+++ app/src/main/java/collegework/calculator2/MainFragment.java
@@ -0,0 +1,24 @@
+package collegework.calculator2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import collegework.calculator2.R;
+
+public class MainFragment extends Fragment{
+
+    EditText editText1,editText2;
+    static String input;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        return view;
+    }
+}
